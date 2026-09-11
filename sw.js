f93b7b2013735fc45f0526b8c9431bc63eef6105
carsen-hintz <carsenrhintz@gmail.com>
2026-09-11 07:30:36 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * activate, so a version bump is what forces every phone to take the update.
  */
 
-const CACHE = 'jobsite-logger-v5';
+const CACHE = 'jobsite-logger-v7';
 
 const SHELL = [
   './',
